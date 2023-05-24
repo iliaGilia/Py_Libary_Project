ebc0e4ea26a08cc505d594711dccaa4728b4bc10
Syltras <127669591+Syltras@users.noreply.github.com>
2023-05-24 15:57:00 +0300

    Add files via upload

--- Back/app.py
+++ Back/app.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite3'
@@ -12,7 +13,7 @@ db = SQLAlchemy(app)
 
 # Model
 class Book(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)  # Modified column name
+    id = db.Column('id', db.Integer, primary_key=True)
     book_name = db.Column(db.String(100))
     book_author = db.Column(db.String(50))
     book_year = db.Column(db.String(10))
@@ -35,7 +36,7 @@ class Book(db.Model):
         }
 
 class Customer(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)  # Added primary key
+    id = db.Column('id', db.Integer, primary_key=True)
     cust_name = db.Column(db.String(100))
     cust_city = db.Column(db.String(50))
     cust_age = db.Column(db.Integer)
@@ -82,10 +83,16 @@ def get_books():
     json_data = json.dumps(books_list)
     return json_data
 
-@app.route("/customers")
+@app.route("/customers", methods=['GET'])
 def get_customers():
     customers_list = [customer.to_dict() for customer in Customer.query.all()]
     json_data = json.dumps(customers_list)
+    return json_data
+
+@app.route("/loans", methods=['GET'])
+def get_loans():
+    loans_list = [loan.to_dict() for loan in Loan.query.all()]
+    json_data = json.dumps(loans_list)
     return json_data
 
 @app.route("/books/del/<id>", methods=['DELETE'])
@@ -99,6 +106,13 @@ def del_book(id=-1):
 def del_customer(id=-1):
     customer = Customer.query.get_or_404(id)
     db.session.delete(customer)
+    db.session.commit()
+    return {"delete": "success"}
+
+@app.route("/loans/del/<id>", methods=['DELETE'])
+def del_loan(id=-1):
+    loan = Loan.query.get_or_404(id)
+    db.session.delete(loan)
     db.session.commit()
     return {"delete": "success"}
 
@@ -120,6 +134,17 @@ def upd_customer(id=-1):
     customer.cust_name = data['cust_name']
     customer.cust_city = data['cust_city']
     customer.cust_age = data['cust_age']
+    db.session.commit()
+    return {"update": "success"}
+
+@app.route("/loans/upd/<id>", methods=['PUT'])
+def upd_loan(id=-1):
+    loan = Loan.query.get(id)
+    data = request.get_json()
+    loan.cust_id = data['cust_id']
+    loan.book_id = data['book_id']
+    loan.loanDate = data['loanDate']
+    loan.returnDate = data['returnDate']
     db.session.commit()
     return {"update": "success"}
 
@@ -148,6 +173,31 @@ def new_customer():
     db.session.add(new_customer)
     db.session.commit()
     return "A new customer record was created."
+
+@app.route('/loans/new', methods=['POST'])
+def new_loan():
+    data = request.get_json()
+    cust_id = data['cust_id']
+    book_id = data['book_id']
+    book_type = data['book_type']
+
+    # Set loanDate to today's date
+    loanDate = datetime.now().strftime('%Y-%m-%d')
+
+    # Set returnDate based on book_type
+    if book_type == '1':
+        returnDate = (datetime.now() + timedelta(days=2)).strftime('%Y-%m-%d')
+    elif book_type == '2':
+        returnDate = (datetime.now() + timedelta(days=5)).strftime('%Y-%m-%d')
+    elif book_type == '3':
+        returnDate = (datetime.now() + timedelta(days=10)).strftime('%Y-%m-%d')
+    else:
+        return "Invalid book type."
+
+    new_loan = Loan(cust_id, book_id, loanDate, returnDate)
+    db.session.add(new_loan)
+    db.session.commit()
+    return "A new loan record was created."
 
 if __name__ == "__main__":
     with app.app_context():
